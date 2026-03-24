cc52d2eb99efbb30667320b4044f1d9598e80070
aarthisuresh2402 <aarthiibm24@gmail.com>
2026-03-24 18:18:37 +0530

    Add files via upload

---
+++ aaru.py
@@ -0,0 +1,93 @@
+
+
+
+# IF condition
+
+a= 725
+n= 25
+if a>n:
+    print ( " HELLO WORLD ")
+
+f= 8576
+j= 4877
+if f<j:
+    print ( " MY NAME IS AARTHI")
+
+k= 4768.945
+g= 4877.878
+if g>k:
+    print (" HELLO PYTHON ")
+
+j= 55
+g= 55
+if j==g:
+    print ( " Happy Morning")
+
+
+# IF-ELSE COndition
+
+s= 8888
+a= 9999
+if s<a:
+    print (" I am here")
+else :
+    print (" I am not here ")
+
+a= 7656
+n= 8688
+if a<n:
+    print ( " HI ")
+else :
+    print (" BYE ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''# IF-ELSE STATEMENT
+
+# <= condition
+m = 200
+n = 100
+if m<=n:
+    print ("it is True")
+else :
+    print ( " it is false")
+
+#>= condition
+m = 685
+n = 588
+if m>=n:
+    print (" It is True ")
+else :
+    print (" It is false ")
+    
+# == condition
+a = 8765
+n = 8765
+if a==n:
+    print (" SHE IS A BEAUTIFUL GIRL ")
+else:
+    print (" SHE IS UGLY ")'''
+
+
+    
+    
+
+
